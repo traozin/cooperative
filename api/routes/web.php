8e0ff70ea31464d5a43177ad4242d81c104ec198
Antônio Neto <acm.neto1999@gmail.com>
2025-07-21 23:35:58 -0300

    :sparkles: feat: adicionar rotas da API para gerenciamento de cooperados

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,21 +1,7 @@
 <?php
 
-use App\Http\Controllers\CooperadoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('api.ping');
 });
-
-Route::prefix('api/v1')->group(function () {    
-    Route::get('ping', function () {
-        $start = hrtime(true);
-        usleep(1000);
-        $elapsed = (hrtime(true) - $start) / 1e+6;
-        return response()->json(['message' => 'Pong! ' . round($elapsed, 2) . 'ms']);
-    })->name('api.ping');
-
-    // Define API resource routes for cooperados
-    Route::apiResource('cooperados', CooperadoController::class);
-});
-
