c9a763ed63608d0e178970ad356fedfdcd61e70c
Antônio Neto <acm.neto1999@gmail.com>
2025-07-21 21:49:19 -0300

    :wrench: chore: adicionando orquestração de containers

---
+++ api/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+until nc -z "$DB_HOST" 3306; do
+  echo "Aguardando MySQL em $DB_HOST:3306..."
+  sleep 10
+done
+
+echo "MySQL está pronto. Continuando..."
+
+if [ ! -f .env ]; then
+  cp .env.example .env
+fi
+
+composer install --no-interaction --optimize-autoloader
+php artisan key:generate
+php artisan migrate
+php artisan db:seed
+
+chmod -R 775 storage bootstrap/cache
+
+exec "$@"
